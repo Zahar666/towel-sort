bdede6043e73b7c1b375e4ba03e076cbb215bbd2
Zahar Bystrok <zbystrok.jr@gmail.com>
2020-10-08 18:19:18 +0300

    init: start pets towel-sort-task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length == 0) {
+    return []
+  } else {
+    let arr = []
+    for (let i = 0; i < matrix.length; i++) { 
+      if (i % 2 == 0 || i == 0) {
+        for (let x = 0; x < matrix[i].length; x++) {
+          arr.push(matrix[i][x])
+        }
+      } else {
+        for (let x = matrix[i].length - 1; x >= 0; x--) {
+          arr.push(matrix[i][x])
+        }
+      }
+    }
+    return arr
+  }
 }
